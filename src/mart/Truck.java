eb0d1a8daefee046a9734b673d9cb472a5797f6e
Jon M <jon.meyer91@gmail.com>
2018-05-10 22:25:50 +1000

    Added Truck and RefrigeratedTruck
    Added base code.

--- src/mart/Truck.java
+++ src/mart/Truck.java
@@ -1,8 +1,17 @@
 package mart;
 
+import java.util.*;
+
 public abstract class Truck {
 
-	public Truck() {
+	double cost;
+	int cargoCapacity;
+	HashMap<Item, Integer> cargo;
+	
+	public Truck(double cost, int cargoCapacity, HashMap<Item, Integer> cargo) {
+		this.cost = cost;
+		this.cargoCapacity = cargoCapacity;
+		this.cargo = cargo;
 	}
 
 }
