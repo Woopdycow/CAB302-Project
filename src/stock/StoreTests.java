918d84985dae4ca02da4d85c630d4a6d57f38d97
Jon M <jon.meyer91@gmail.com>
2018-05-16 18:30:53 +1000

    Added Tests to StoreTests

--- src/stock/StoreTests.java
+++ src/stock/StoreTests.java
@@ -6,9 +6,20 @@ import org.junit.Test;
 
 public class StoreTests {
 	
+	Manifest myManifest;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testManifestLoad() {
+		List<Truck> nignog = new ArrayList<Truck>;
+		// for each line in the csv
+		// if line == >Refrigerated then
+		// create new ColdTruck
+		// else if line == >Ordinary then
+		// create new NormalTruck
+		// else add item and value to most recently created truck
+		myManifest = new Manifest();
+		
+		
 	}
 
 }
