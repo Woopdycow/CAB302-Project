58297c26994b6a8e58f32e107df599cbf8c2b60c
Bryan Kassulke <bryan.kassulke@gmail.com>
2018-05-16 17:50:00 +1000

    Added Accessor Methods to Trucks

--- src/stock/Item.java
+++ src/stock/Item.java
@@ -1,13 +1,56 @@
 package stock;
 
 public class Item {
+	private String name;
+	private double cost;
+	private double price;
+	private int reorderPoint;
+	private int reorderAmount;
+	private double temp;
 
 	/**
 	 * Constructor Method
 	 * @author Bryan Kassulke
 	 */
-	public Item() {
-		// TODO Auto-generated constructor stub
+	public Item(String name, double cost, double price, int reorderPoint, int reorderAmount) {
+		name = this.name;
+		cost = this.cost;
+		price = this.price;
+		reorderPoint = this.reorderPoint;
+		reorderAmount = this.reorderAmount;
+	}
+	
+	public Item(String name, double cost, double price, int reorderPoint, int reorderAmount, double temp) {
+		name = this.name;
+		cost = this.cost;
+		price = this.price;
+		reorderPoint = this.reorderPoint;
+		reorderAmount = this.reorderAmount;
+		temp = this.temp;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getCost() {
+		return cost;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public int getReorderPoint() {
+		return reorderPoint;
+	}
+	
+	public int getReorderAmount() {
+		return reorderAmount;
+	}
+	
+	public double getTemp() {
+		return temp;
 	}
 
 }
