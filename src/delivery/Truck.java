2dbe9d97502eca993855f998695306abb45954e9
Bryan Kassulke <bryan.kassulke@gmail.com>
2018-05-27 04:39:54 +1000

    Added more Javadoc Comments

--- src/delivery/Truck.java
+++ src/delivery/Truck.java
@@ -10,7 +10,7 @@ public abstract class Truck {
 	String type;
 	
 	/**
-	 * Constructor Method
+	 * Constructor Method for Truck
 	 * @author Jonathon Meyer
 	 */	
 	public Truck() {};
@@ -19,14 +19,29 @@ public abstract class Truck {
 	
 	public abstract void setCargo(Stock stock) throws TruckOverloadException;
 	
+	/**
+	 * Gets the Truck's cargo Stock object.
+	 * @author Jonathon Meyer
+	 * @return Returns truck cargo as Stock Object.
+	 */
 	public Stock getCargo() {
 		return cargo;
 	}
 	
+	/**
+	 * Gets the maximum capacity of the truck.
+	 * @author Jonathon Meyer
+	 * @return Returns integer value of the trucks capacity.
+	 */
 	public int getCapacity() {
 		return cargoCapacity;
 	}
 	
+	/**
+	 * Gets the type of truck. Refrigerated or Ordinary.
+	 * @author Jonathon Meyer
+	 * @return Returns the type of truck as a string.
+	 */
 	public String getType() {
 		return type;
 	}
