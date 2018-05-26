1c6db9723e2e85567d2d35e49423c1dcf4d433f3
Bryan Kassulke <bryan.kassulke@gmail.com>
2018-05-27 04:34:15 +1000

    Added Javadoc Comments for Item Stock and Store

--- src/stock/Item.java
+++ src/stock/Item.java
@@ -10,8 +10,13 @@ public class Item {
 	private boolean dryGoods;
 
 	/**
-	 * Constructor Method
+	 * Constructor Method for when no temperature is specified.
 	 * @author Bryan Kassulke
+	 * @param name The name of the item.
+	 * @param cost Cost(Manufacturing) price of the item.
+	 * @param price Sell price of the item.
+	 * @param reorderPoint Threshold for items to be reordered.
+	 * @param reorderAmount Quantity of items to order when below reorder threshold.
 	 */
 	public Item(String name, double cost, double price, int reorderPoint, int reorderAmount) {
 		this.name = name;
@@ -19,9 +24,20 @@ public class Item {
 		this.price = price;
 		this.reorderPoint = reorderPoint;
 		this.reorderAmount = reorderAmount;
+		this.temp = 11.0;
 		this.dryGoods = true;
 	}
 	
+	/**
+	 * Constructor Method for when temperature is specified.
+	 * @author Bryan Kassulke
+	 * @param name The name of the item.
+	 * @param cost Cost(Manufacturing) price of the item.
+	 * @param price Sell price of the item.
+	 * @param reorderPoint Threshold for items to be reordered.
+	 * @param reorderAmount Quantity of items to order when below reorder threshold.
+	 * @param temp Temperature of which the item must be stored below.
+	 */
 	public Item(String name, double cost, double price, int reorderPoint, int reorderAmount, double temp) {
 		this.name = name;
 		this.cost = cost;
@@ -32,30 +48,58 @@ public class Item {
 		this.dryGoods = false;
 	}
 	
+	/**
+	 * @author Bryan Kassulke
+	 * @return Returns name of the item.
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * @author Bryan Kassulke
+	 * @return Returns cost(manufacturing) of the item.
+	 */
 	public double getCost() {
 		return cost;
 	}
 	
+	/**
+	 * @author Bryan Kassulke
+	 * @return Returns sell price of the item.
+	 */
 	public double getPrice() {
 		return price;
 	}
 	
+	/**
+	 * @author Bryan Kassulke
+	 * @return Returns reorder point of the item.
+	 */
 	public int getReorderPoint() {
 		return reorderPoint;
 	}
 	
+	/**
+	 * @author Bryan Kassulke
+	 * @return Returns reorder amount of the item.
+	 */
 	public int getReorderAmount() {
 		return reorderAmount;
 	}
 	
+	/**
+	 * @author Bryan Kassulke
+	 * @return Returns maximum transport temperature of the item.
+	 */
 	public double getTemp() {
 		return temp;
 	}
 	
+	/**
+	 * @author Bryan Kassulke
+	 * @return Returns boolean of whether the item is dry goods or not.
+	 */
 	public boolean isDryGoods() {
 		return dryGoods;
 	}
