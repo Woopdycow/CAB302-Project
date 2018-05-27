191011c0bbae37dfb86ee6728f6adcc73fc2a073
Bryan Kassulke <bryan.kassulke@gmail.com>
2018-05-27 23:13:22 +1000

    Merged ManifestReader

--- src/CSV/ManifestReader.java
+++ src/CSV/ManifestReader.java
@@ -40,16 +40,6 @@ public class ManifestReader {
 		Store myStore = Store.getInstance();
 
 		Manifest delivery = new Manifest();
-<<<<<<< HEAD
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-			//Print each line to an array
-			List<String> lines = new ArrayList<>();
-			for (int i = 0; (line = br.readLine()) != null; i++) {
-				lines.add(line);
-			}
-=======
->>>>>>> 61db207ad8e445d87397a0b86282dbb3b17c1677
 
 		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
 			Item newItem;
@@ -76,26 +66,7 @@ public class ManifestReader {
 					} else {
 						throw new CSVFormatException("Invalid truck type.");
 					}
-<<<<<<< HEAD
-					Stock cargo = new Stock();
-					for (int j = i + 1; j < items + i + 1; j++) {
-						String[] itemInfo = lines.get(j).split(csvSplitBy);
-						int quantity = new Integer(itemInfo[1]);
-						item = myStore.getItemByName(itemInfo[0]);
-						//System.out.println(myStore.getInstance().getItemByName("rice").getCost());
-						//System.out.println(item.getPrice());
-						cargo.addItem(item, quantity);
-						System.out.println(itemInfo[0]);
-					}
-					if (cargo.getTotal() == 0) {
-						throw new CSVFormatException("No items for truck cargo.");
-					}
-					newTruck.setCargo(cargo);
-					delivery.addTruck(newTruck);
-				}
-			} else {
-				throw new CSVFormatException("Not correct manifest format.");
-=======
+
 				} else {
 					String[] itemInfo = line.split(csvSplitBy);
 					name = new String (itemInfo[0]);
@@ -107,14 +78,10 @@ public class ManifestReader {
 			}
 			for (Truck k : trucks) {
 				delivery.addTruck(k);
->>>>>>> 61db207ad8e445d87397a0b86282dbb3b17c1677
 			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		for (Truck k : delivery.getTrucks()) {
-			
 		}
 		return delivery;
 	}
